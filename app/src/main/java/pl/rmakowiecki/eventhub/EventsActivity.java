374f812be831fa55d8f543282a3876b0ec44df32
Haydart <makowiecki.radoslaw@gmail.com>
2017-03-12 15:50:58 +0100

    Add sample activity start text (#3)

--- app/src/main/java/pl/rmakowiecki/eventhub/EventsActivity.java
+++ app/src/main/java/pl/rmakowiecki/eventhub/EventsActivity.java
@@ -1,15 +1,26 @@
 package pl.rmakowiecki.eventhub;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 import java.util.List;
+
+import butterknife.BindView;
 import pl.rmakowiecki.eventhub.model.local.Event;
 import pl.rmakowiecki.eventhub.ui.BaseActivity;
 
 public class EventsActivity extends BaseActivity<EventsPresenter> implements EventsView {
 
+    @BindView(R.id.helloworld_text_view)TextView helloWorldTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        helloWorldTextView.setText("Activity has started!");
     }
 
     @Override
