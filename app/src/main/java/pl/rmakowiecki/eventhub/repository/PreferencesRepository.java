ccad271306a33ba533c2058dd4a97757d86e0a61
Wolffenstein <dominik.kozlowski.dk@gmail.com>
2017-03-16 08:09:10 +0100

    Add missing TODO and Observable.empty() in PreferencesRepository.java

--- app/src/main/java/pl/rmakowiecki/eventhub/repository/PreferencesRepository.java
+++ app/src/main/java/pl/rmakowiecki/eventhub/repository/PreferencesRepository.java
@@ -8,26 +8,26 @@ public class PreferencesRepository implements Repository<Preference> {
 
     @Override
     public void add(Preference item) {
-
+        // TODO: 2017-03-16
     }
 
     @Override
     public void add(Iterable<Preference> items) {
-
+        // TODO: 2017-03-16
     }
 
     @Override
     public void update(Preference item) {
-
+        // TODO: 2017-03-16
     }
 
     @Override
     public void remove(Preference item) {
-
+        // TODO: 2017-03-16
     }
 
     @Override
     public Observable<List<Preference>> query(Specification specification) {
-        return null;
+        return Observable.empty();
     }
 }
