d4cb237d5e319effafc1cd5796cefd7e66be6c5e
Haydart <makowiecki.radoslaw@gmail.com>
2017-03-23 11:29:13 +0100

    Add bottom sheet fab behavior

--- app/src/main/java/pl/rmakowiecki/eventhub/ui/events_map_screen/EventsMapActivity.java
+++ app/src/main/java/pl/rmakowiecki/eventhub/ui/events_map_screen/EventsMapActivity.java
@@ -4,17 +4,19 @@ import android.Manifest;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 import butterknife.BindView;
+import butterknife.OnClick;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -42,24 +44,44 @@ public class EventsMapActivity extends BaseActivity<EventsMapPresenter> implemen
         GoogleMap.OnPoiClickListener,
         NavigationView.OnNavigationItemSelectedListener {
 
-
     private static final float DEFAULT_MAP_ZOOM = 17f;
     private static final float MIN_MAP_ZOOM = 9f;
     private static final long PERMISSION_GRANTING_DELAY = 250;
     public static final int MAP_PADDING_TOP = 64;
-    private static final int BOTTOM_SHEET_MAP_PADDING = 240;
+    private static final int BOTTOM_SHEET_MAP_PADDING = 256;
+
+    @BindView(R.id.drawer_layout) DrawerLayout drawer;
+    @BindView(R.id.nav_view) NavigationView navigationView;
+    @BindView(R.id.bottom_sheet_fab) FloatingActionButton bottomSheetFab;
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.map_bottom_sheet) FrameLayout bottomSheetLayout;
 
     private GoogleMap googleMap;
     private Marker mapClickMarker;
     private BottomSheetBehavior bottomSheetBehavior;
-    @BindView(R.id.drawer_layout) DrawerLayout drawer;
-    @BindView(R.id.nav_view) NavigationView navigationView;
 
+    @OnClick(R.id.bottom_sheet_fab)
+    public void onBottomSheetFabClicked() {
+        // TODO: 23/03/2017 remove debug
+        Toast.makeText(this, "Fab click", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.map_bottom_sheet)
+    public void onBottomSheetClicked() {
+        // TODO: 23/03/2017 remove debug
+        Toast.makeText(this, "Bottom sheet click", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setSupportActionBar(toolbar);
+        checkLocationPermissions();
+        initMapBottomSheet();
+        initNavigationDrawer();
+    }
 
+    private void checkLocationPermissions() {
         new RxPermissions(this)
                 .request(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
                 .delay(PERMISSION_GRANTING_DELAY, TimeUnit.MILLISECONDS)
@@ -71,49 +93,34 @@ public class EventsMapActivity extends BaseActivity<EventsMapPresenter> implemen
                         // TODO: 14/03/2017 implement user revoke reaction
                     }
                 });
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    }
 
+    private void initNavigationDrawer() {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
 
-        View bottomSheet = findViewById(R.id.place_bottom_sheet);
-        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+    private void initMapBottomSheet() {
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetLayout);
         bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
-        // TODO: 19/03/2017 refactor
         bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
             public void onStateChanged(@NonNull View bottomSheet, int newState) {
-                switch (newState) {
-                    case BottomSheetBehavior.STATE_DRAGGING:
-                        Log.i("BottomSheetCallback", "BottomSheetBehavior.STATE_DRAGGING");
-                        break;
-                    case BottomSheetBehavior.STATE_SETTLING:
-                        Log.i("BottomSheetCallback", "BottomSheetBehavior.STATE_SETTLING");
-                        break;
-                    case BottomSheetBehavior.STATE_EXPANDED:
-                        Log.i("BottomSheetCallback", "BottomSheetBehavior.STATE_EXPANDED");
-                        break;
-                    case BottomSheetBehavior.STATE_COLLAPSED:
-                        Log.i("BottomSheetCallback", "BottomSheetBehavior.STATE_COLLAPSED");
-                        break;
-                    case BottomSheetBehavior.STATE_HIDDEN:
-                        Log.i("BottomSheetCallback", "BottomSheetBehavior.STATE_HIDDEN");
-                        break;
+                if (BottomSheetBehavior.STATE_DRAGGING == newState) {
+                    bottomSheetFab.animate().scaleX(0).scaleY(0).setDuration(300).start();
+                } else if (BottomSheetBehavior.STATE_COLLAPSED == newState) {
+                    bottomSheetFab.animate().scaleX(1).scaleY(1).setDuration(300).start();
                 }
             }
 
             @Override
             public void onSlide(@NonNull View bottomSheet, float slideOffset) {
-                Log.i("BottomSheetCallback", "slideOffset: " + slideOffset);
+                //no-op
             }
         });
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        navigationView.setNavigationItemSelectedListener(this);
-
     }
 
     @Override
@@ -154,7 +161,8 @@ public class EventsMapActivity extends BaseActivity<EventsMapPresenter> implemen
 
     @Override
     public void onPoiClick(PointOfInterest pointOfInterest) {
-        // TODO: 19/03/2017 implement
+        Toast.makeText(this, "Point of interest click", Toast.LENGTH_SHORT).show();
+        // TODO: 23/03/2017 remove debug
     }
 
     @Override
@@ -177,8 +185,7 @@ public class EventsMapActivity extends BaseActivity<EventsMapPresenter> implemen
     @Override
     public void showMapClickMarker(LocationCoordinates locationCoordinates) {
         mapClickMarker = googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(locationCoordinates.getLatitude(), locationCoordinates.getLongitude()))
-                .title(locationCoordinates.toString()));
+                .position(new LatLng(locationCoordinates.getLatitude(), locationCoordinates.getLongitude())));
     }
 
     @Override
@@ -190,6 +197,7 @@ public class EventsMapActivity extends BaseActivity<EventsMapPresenter> implemen
     public void showBottomSheet() {
         if (bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_HIDDEN) {
             bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            bottomSheetFab.setVisibility(View.VISIBLE);
         }
     }
 
@@ -197,6 +205,7 @@ public class EventsMapActivity extends BaseActivity<EventsMapPresenter> implemen
     public void hideBottomSheet() {
         if (bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_COLLAPSED) {
             bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+            bottomSheetFab.setVisibility(View.INVISIBLE);
         }
     }
 
