5eaeae92cb766e98c10b6e1b7881cf71d44eb993
marcinv1t0 <m1perlinski@gmail.com>
2017-04-23 17:56:58 +0200

    Add event informationt transformation and layout fixes, fix tab titles bug

--- app/src/main/java/pl/rmakowiecki/eventhub/ui/screen_event_calendar/EventsFragmentAdapter.java
+++ app/src/main/java/pl/rmakowiecki/eventhub/ui/screen_event_calendar/EventsFragmentAdapter.java
@@ -1,26 +1,22 @@
 package pl.rmakowiecki.eventhub.ui.screen_event_calendar;
 
-import android.content.Context;
+import android.app.Activity;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import butterknife.BindString;
-import pl.rmakowiecki.eventhub.R;
 
 /**
  * Created by m1per on 09.04.2017.
  */
 public class EventsFragmentAdapter extends FragmentPagerAdapter {
 
-
     private final int PAGE_COUNT = 2;
-    @BindString(R.string.tab_title_all_events) String allEventsTabTitle;
-    @BindString(R.string.tab_title_my_events) String myEventsTabTitle;
 
-    private String tabTitles[] = new String[] { allEventsTabTitle, myEventsTabTitle};
+    private String tabTitles[];
 
-    public EventsFragmentAdapter(FragmentManager fm, Context context) {
+    public EventsFragmentAdapter(FragmentManager fm, Activity activity, String[] titles) {
         super(fm);
+        tabTitles = titles;
     }
 
     @Override
