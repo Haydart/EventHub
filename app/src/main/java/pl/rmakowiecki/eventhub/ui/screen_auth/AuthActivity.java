7bf7a07f05454d440011d4877b6a212f46c0192d
Radosław Makowiecki <makowiecki.radoslaw@gmail.com>
2017-05-07 17:28:42 +0200

    Add auth screen (#50)

--- app/src/main/java/pl/rmakowiecki/eventhub/ui/screen_auth/AuthActivity.java
+++ app/src/main/java/pl/rmakowiecki/eventhub/ui/screen_auth/AuthActivity.java
@@ -1,14 +1,36 @@
 package pl.rmakowiecki.eventhub.ui.screen_auth;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.TextInputLayout;
+import android.view.View;
+import android.widget.FrameLayout;
+import butterknife.BindView;
+import butterknife.OnClick;
 import pl.rmakowiecki.eventhub.R;
+import pl.rmakowiecki.eventhub.ui.BaseActivity;
 
-public class AuthActivity extends AppCompatActivity {
+public class AuthActivity extends BaseActivity {
+    @BindView(R.id.input_layout_repeat_password) TextInputLayout passwordRepeatInputLayout;
+    @BindView(R.id.login_bottom_layout) FrameLayout loginOptionsBottomLayout;
+
+    // TODO: 07/05/2017 refactor to mvp
+    @OnClick(R.id.auth_action_button)
+    public void onButtonClicked() {
+        if (passwordRepeatInputLayout.getVisibility() == View.GONE) {
+            passwordRepeatInputLayout.setVisibility(View.VISIBLE);
+            loginOptionsBottomLayout.setVisibility(View.GONE);
+        } else {
+            passwordRepeatInputLayout.setVisibility(View.GONE);
+            loginOptionsBottomLayout.setVisibility(View.VISIBLE);
+        }
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_auth);
+    protected void initPresenter() {
+        presenter = new AuthPresenter();
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_auth;
     }
 }
