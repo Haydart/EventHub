281782e23630500347828ef5a6cf65ff20c144c3
marcinv1t0 <m1perlinski@gmail.com>
2017-06-03 01:02:23 +0200

    Remove unused imports

--- app/src/main/java/pl/rmakowiecki/eventhub/ui/screen_event_calendar/CalendarActivity.java
+++ app/src/main/java/pl/rmakowiecki/eventhub/ui/screen_event_calendar/CalendarActivity.java
@@ -6,7 +6,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
